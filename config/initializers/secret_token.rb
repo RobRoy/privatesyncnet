98eb312797717415232d3fa6b94dc5a76723310a
Robert Schmidl <robert@midtones.de>
2013-05-06 16:38:09 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Privatesyncnet::Application.config.secret_token = '86af091da4da11e72a2e30b6b71ab0daa3e4cd0770667648a0f157fd0bc54b5622ea0ddcbb2499a2118b90a1b96edb3a76c892d4a72fe2bf79251cb1d0a767e4'
